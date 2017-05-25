0615e4b4328f634fcd440b0703369efb1ec5c0b4
Lisa Yan <lisasyan@gmail.com>
2017-05-24 20:00:07 -0700

    Pushing new bonus projects.

---
+++ starter/ClickNStuff/src/ClickNJump.java
@@ -0,0 +1,28 @@
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.RandomGenerator;
+
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+
+
+public class ClickNJump extends GraphicsProgram {
+	/** Width and height of application window in pixels */
+    public static int APPLICATION_WIDTH = 600;
+    public static int APPLICATION_HEIGHT = 400;
+    
+    /** Animation loop delay **/
+    public static int DELAY = 50;
+    public static int JUMPTIME = 2000;
+    
+    /** Other constants **/
+    public static int SIZE = 50;
+    public static int MOVE_AMT = 10;
+    public static int KAREL_OFFSET = 100;
+    
+	public void run() {
+		// your code here...
+	}
+	
+}
---
+++ starter/ClickNStuff/src/ClickNPause.java
@@ -0,0 +1,21 @@
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.RandomGenerator;
+
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+
+
+public class ClickNPause extends GraphicsProgram {
+	/** Width and height of application window in pixels */
+    public static int APPLICATION_WIDTH = 600;
+    public static int APPLICATION_HEIGHT = 400;
+    
+    /** Animation loop delay **/
+    public static int DELAY = 50;
+    
+	public void run() {
+		/* YOUR CODE HERE */
+	}
+}
