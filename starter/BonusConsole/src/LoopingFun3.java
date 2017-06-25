d9eee7604ead45aeef769329f137a96c84983132
Lisa Yan <lisasyan@gmail.com>
2017-06-25 16:47:57 -0700

    Added new bonus loop problems and cleaned up git.

---
+++ starter/BonusConsole/src/LoopingFun3.java
@@ -0,0 +1,14 @@
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.RandomGenerator;
+
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+
+public class LoopingFun3 extends ConsoleProgram {
+	
+	public void run() {
+		/* You fill this in */
+	}
+}
