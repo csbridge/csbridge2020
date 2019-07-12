3825e186aa3e43449f50c4e0dada976ca2a5153d
Nick Troccoli <nicktroccoli28@gmail.com>
2019-07-12 16:37:13 +0200

    Updated compile script version
    Now will ignore compiling any files ending in .ptl, in any location in the templates/ folder.  All .html files will be compiled.

--- compile.py
+++ compile.py
@@ -1,81 +1,176 @@
+#!/usr/bin/env python
+
+'''
+FILE: compile.py
+----------------
+Template compiler that compiles all .html template files in the TEMPLATE_DIR
+directory below (excluding .ptl files, which are partial templates), and outputs
+with the same filenames to the OUTPUT_DIR directory.
+Example usage:
+
+> python compile.py --output_dir docs
+
+Compiles all template files using local paths, and outputs the compiled files to
+the docs directory.  The compiled files in docs/ have the same directory structure
+as in the TEMPLATE_DIR directory.
+
+> python compile.py
+
+Compiles all template files and outputs the compiled files to
+the OUTPUT_DIR below.
+----------------
+'''
+
 from util.bottle.bottle import SimpleTemplate
-
+import json
 import os.path
+import sys
 
-IGNORE_DIRS = [
-    'parts'
-]
+
 TEMPLATE_DIR = 'templates'
-HTML_DIR = 'docs'
 
-class Compiler(object):
+# Use the --output_dir flag to optionally specify where compiled files go
+if '--output_dir' in sys.argv:
+    OUTPUT_DIR = sys.argv[sys.argv.index('--output_dir') + 1]
+else:
+    OUTPUT_DIR = 'docs'
 
-    # Function: Run
-    # -------------
-    # This function compiles all the html files (recursively)
-    # from the templates dir into the current folder. Folder
-    # hierarchy is preserved
-    def run(self):
-        templateFilePaths = self.getTemplateFilePaths('')
-        for templateFilePath in templateFilePaths:
-            self.compileTemplate(templateFilePath)
+'''
+FUNCTION: compile
+-----------------
+Parameters: NA
+Returns: NA
 
-    #####################
-    # Private Helpers
-    #####################
+This function compiles all the html files (recursively)
+from the templates dir into the output folder. Folder
+hierarchy is preserved.
+-----------------
+''' 
+def compile():
+    # Compile all templates
+    templateFilePaths = getTemplateFilePaths('')
+    print("\nCompiling:\n----------")
+    for templateFilePath in templateFilePaths:
+        print("Compiling " + templateFilePath + "...")
+        outputPath = compileTemplate(templateFilePath)
+        print(templateFilePath + " -> " + outputPath)
 
-    def compileTemplate(self, relativePath):
-        print(relativePath)
-        pathToRoot = self.getPathToRoot(relativePath)
-        templateFilePath = os.path.join(TEMPLATE_DIR, relativePath)
-        templateText = open(templateFilePath).read()
-        compiledHtml = SimpleTemplate(templateText).render(pathToRoot = pathToRoot)
-        htmlFilePath = os.path.join(HTML_DIR, relativePath)
-        self.makePath(htmlFilePath)
-        fileName, fileExtension = os.path.splitext(relativePath)
-        compiledHtml = compiledHtml.encode('utf8')
-        open(htmlFilePath, 'wb').write(compiledHtml)
+    print("\nDONE.\n")
 
-    def makePath(self, path):
-        dirPath = os.path.dirname(path)
-        if dirPath == '': return
-        if not os.path.exists(dirPath):
-            os.makedirs(dirPath)
-        
-    def getPathToRoot(self, relativePath):
-        dirs = self.splitDirs(relativePath)
-        depth = len(dirs) - 1
-        pathToRoot = ''
-        for _ in range(depth):
-            pathToRoot += '../'
-        return pathToRoot
+'''
+FUNCTION: getTemplateFilePaths
+------------------------------
+Parameters:
+    templateRoot - the folder within TEMPLATE_DIR to get file paths for
 
-    def splitDirs(self, filePath):
-        if filePath == '': return []
-        rootPath, last = os.path.split(filePath)
-        rootDirs = self.splitDirs(rootPath)
-        rootDirs.append(last)
-        return rootDirs
+Returns: a list of .html template file paths from within the given directory
+within TEMPLATE_DIR.  Ignores .ptl files, which are partial templates.
+------------------------------
+'''
+def getTemplateFilePaths(templateRoot):
+    paths = []
+    templateDirPath = os.path.join(TEMPLATE_DIR, templateRoot)
+    for fileName in os.listdir(templateDirPath):
+        filePath = os.path.join(templateRoot, fileName)
+        templateFilePath = os.path.join(TEMPLATE_DIR, filePath)
 
-    def isTemplateFile(self, fileName):
-        extension = os.path.splitext(fileName)[1]
-        return extension == '.html'
+        # Recurse if it's a directory, add if it's a template file
+        if os.path.isdir(templateFilePath):
+            childPaths = getTemplateFilePaths(filePath)
+            for childPath in childPaths:
+                paths.append(childPath)
+        elif isTemplateFile(fileName):
+            paths.append(filePath)
 
-    def getTemplateFilePaths(self, root):
-        if root in IGNORE_DIRS: return []
-        paths = []
-        templateDirPath = os.path.join(TEMPLATE_DIR, root)
-        for fileName in os.listdir(templateDirPath):
-            filePath = os.path.join(root, fileName)
-            templateFilePath = os.path.join(TEMPLATE_DIR, filePath)
-            if os.path.isdir(templateFilePath):
-                childPaths = self.getTemplateFilePaths(filePath)
-                for childPath in childPaths:
-                    paths.append(childPath)
-            elif self.isTemplateFile(fileName):
-                paths.append(filePath)
-        return paths
+    return paths
+
+'''
+FUNCTION: isTemplateFile
+------------------------
+Parameters:
+    fileName - the fileName to check is a template file
+
+Returns: whether or not the given filename is a template file (ends with .html)
+------------------------
+'''
+def isTemplateFile(fileName):
+    extension = os.path.splitext(fileName)[1]
+    return extension == '.html'
+
+'''
+FUNCTION: compileTemplate
+-------------------------
+Parameters:
+    relativePath - the path within TEMPLATE_DIR of the template file to compile
+
+Returns: the path of the saved, compiled template file.
+
+Compiles the given template file.  Saves the compiled template to relativePath 
+in the OUTPUT_DIR directory.
+-------------------------
+'''
+def compileTemplate(relativePath):
+    pathToRoot = getPathToRootFrom(relativePath)
+    filePath = os.path.join(TEMPLATE_DIR, relativePath)
+    templateText = open(filePath).read()
+    compiledHtml = SimpleTemplate(templateText).render(pathToRoot=pathToRoot)
+    compiledHtml = compiledHtml.encode('utf8')
+
+    relativePath = os.path.join(OUTPUT_DIR, relativePath)
+    makePath(relativePath)
+    open(relativePath, 'wb').write(compiledHtml)
+    return relativePath
+
+'''
+FUNCTION: getPathToRootFrom
+---------------------------
+Parameters:
+    relativePath - the path to start at when calculating the path to the root
+
+Returns: the relative path to the root directory from the given relativePath.
+    Concatenates "../" for each level down from the root.
+------------------------------
+'''
+def getPathToRootFrom(relativePath):
+    depth = depthFromRoot(relativePath)
+    pathToRoot = ''.join(['../' for i in range(depth)])
+    return pathToRoot
+
+'''
+FUNCTION: depthFromRoot
+-----------------------
+Parameters:
+    filePath - the path for which to calculate the depth
+
+Returns: the number of levels filePath is from the root level.
+    E.g. 'index.html' -> 0
+         'stuff/index.html' -> 1
+         'stuff/moreStuff/index.html' -> 2
+-----------------------
+'''
+def depthFromRoot(filePath):
+    rootPath = os.path.dirname(filePath)
+    if len(rootPath) == 0: return 0
+    return depthFromRoot(rootPath) + 1
+    
+'''
+FUNCTION: makePath
+------------------
+Parameters:
+    path - the path to make directories for
+
+Returns: NA
+
+Creates all needed directories in this path for the directory path to exist.
+E.g. if path = 'stuff/moreStuff/index.html' then the stuff and moreStuff
+directories would be created if they did not already exist.
+------------------
+'''
+def makePath(path):
+    dirPath = os.path.dirname(path)
+    if dirPath != '' and not os.path.exists(dirPath):
+        os.makedirs(dirPath)
 
 
 if __name__ == '__main__':
-    Compiler().run()
+    compile()
