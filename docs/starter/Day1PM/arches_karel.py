ed9d888c52ba0a1dadd1414bfd95d43e8a0bef12
Lisa Yan <lisasyan@gmail.com>
2020-07-21 23:37:37 -0700

    Put arches karel back

---
+++ docs/starter/Day1PM/arches_karel.py
@@ -0,0 +1,24 @@
+from karel.stanfordkarel import *
+
+"""
+File: arches_karel.py
+------------------------------
+When you finish writing code in this file, arches_karel should
+solve the "repair the quad" problem (or Charles Bridge, or Efes).
+"""
+
+
+def main():
+    """
+    You should write your code to make Karel do its task in
+    this function. Make sure to delete the 'pass' line before
+    starting to write your own code. You should also delete this
+    comment and replace it with a better, more descriptive one.
+    """
+    pass
+
+
+# There is no need to edit code beyond this point
+
+if __name__ == "__main__":
+    run_karel_program()
