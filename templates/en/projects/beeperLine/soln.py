ff0bc9021db80e2644e9d966d3a98bb2a4782128
Nick Troccoli <nicktroccoli28@gmail.com>
2020-08-03 14:24:56 -0400

    Day 2 AM examples finalized

--- templates/en/projects/beeperLine/soln.py
+++ templates/en/projects/beeperLine/soln.py
@@ -1,7 +1,7 @@
 from karel.stanfordkarel import *
 
 """
-File: beeper_line_karel.py
+File: beeper_line.py
 ------------------------------
 Places a row of beepers on the bottom row of Karel's world.
 Works with any size world.
@@ -13,12 +13,16 @@ def main():
         put_beeper()
         move()
 
-    # the line below is necessary to place the final beeper.
-    # the number of times Karel moves is one less than the number
-    # of times Karel places a beeper (if the world is five squares
-    # wide, we place 5 beepers, but only move 4 times).
+    """
+    the line below is necessary to place the final beeper.
+    the number of times Karel moves is one less than the number
+    of times Karel places a beeper (if the world is five squares
+    wide, we place 5 beepers, but only move 4 times).
+    """
     put_beeper()
+
+
+# There is no need to edit code beyond this point
 
 if __name__ == "__main__":
     run_karel_program()
-
