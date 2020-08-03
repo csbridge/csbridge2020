6b939d6743b6ef94651c4728e5b6a62e784abd54
Nick Troccoli <nicktroccoli28@gmail.com>
2020-08-03 17:20:31 -0400

    fibonacci

--- templates/en/projects/fibonacci/soln.py
+++ templates/en/projects/fibonacci/soln.py
@@ -1,5 +1,5 @@
 """
-File: Fibonacci
+File: fibonacci.py
 --------------------
 This program lists the terms in the Fibonacci sequence up to
 a constant MAX_TERM_VALUE, which is the largest Fibonacci term
@@ -9,15 +9,18 @@ the program will display.
 # Defines the largest term to display
 MAX_TERM_VALUE = 10000
 
+
 def main():
     print("This program lists the Fibonacci sequence.")
-    t1 = 0
-    t2 = 1
-    while t1 <= MAX_TERM_VALUE:
-        print(t1)
-        t3 = t1 + t2
-        t1 = t2
-        t2 = t3
+    current_term = 0
+    next_term = 1
+
+    while current_term <= MAX_TERM_VALUE:
+        print(current_term)
+        term_after_next = current_term + next_term
+        current_term = next_term
+        next_term = term_after_next
+
 
 if __name__ == '__main__':
     main()
