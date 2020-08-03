8517348d94d71d875c1254fec29b8ef97df4e5c5
Nick Troccoli <nicktroccoli28@gmail.com>
2020-08-03 17:36:34 -0400

    favorite number

--- templates/en/projects/favoriteNumber/soln.py
+++ templates/en/projects/favoriteNumber/soln.py
@@ -1,23 +1,26 @@
 """
-File: favoritenumber.py
+File: favorite_number.py
 -------------------
-This program prompts the user to guess its secret
-random number and gives the user hints until it guesses
-it correctly.
+This program prompts the user to guess its secret number and gives the user
+hints until they guess it correctly.
 """
 
+# The number the user must guess
 FAVORITE_NUMBER = 88
 
+
 def main():
-    print("Try and guess my favorite number (between 0 and 100)");
-	
+    print("Try and guess my favorite number (between 0 and 100)")
+
     guess = int(input("Guess: "))
     while guess != FAVORITE_NUMBER:
+
+        # Give them feedback on whether their guess was too low or too high
         if guess < FAVORITE_NUMBER:
             print("Too low")
         else:
             print("Too high")
-            
+
         guess = int(input("Guess: "))
 
     print("Good job!")
