428d07b55fb868d7dbf62f56f0b29f9a838b7e1a
Nick Troccoli <nicktroccoli28@gmail.com>
2020-08-03 14:54:21 -0400

    Updated turkish Karel worked example solutions

--- templates/tr/projects/place100/soln.py
+++ templates/tr/projects/place100/soln.py
@@ -1,18 +1,1 @@
-from karel.stanfordkarel import *
-
-"""
-File: place_100.py
-------------------------
-This program makes karel place a pile of 100 beepers. Good times.
-"""
-
-def main():
-    move()
-    # This for loop will repeat the code inside 100 times.
-    for i in range(100):
-        put_beeper()
-    move()
-
-# There is no need to edit code beyond this point
-if __name__ == "__main__":
-    run_karel_program()
+../../../en/projects/place100/soln.py
