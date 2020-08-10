0ad22ed7891a9862a373413808e6b998e71e7084
Nick Troccoli <nicktroccoli28@gmail.com>
2020-08-09 22:50:30 -0400

    Average

--- templates/en/projects/average/soln.py
+++ templates/en/projects/average/soln.py
@@ -6,12 +6,18 @@ function that takes in two inputs and returns their average.
 The main program calls this function several times with different
 inputs and prints out the results.
 """
+
+
 def main():
     print(average(4, 5))
     print(average(10, 2))
     print(average(-4.1, 9.8))
 
+
 def average(a, b):
+    """
+    Takes in two inputs and returns their average.
+    """
     return (a + b) / 2
 
 
