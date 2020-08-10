fdf32f13bb0cfc99fb113c0dc1f6b2647c56e14c
Nick Troccoli <nicktroccoli28@gmail.com>
2020-08-09 22:32:35 -0400

    Robot face

--- templates/en/projects/robotFace/soln.py
+++ templates/en/projects/robotFace/soln.py
@@ -1,71 +1,110 @@
 """
 File: robot_face.py
 -------------------
-Draws an awesome robot face, with label and different colored
-eyes!!!@#$%#@!
+Draws an awesome robot face with eyes and a mouth, using functions.
+It also includes the text "Robot Face" at the top of the window.
 """
 
 from graphics import Canvas
 
-FACE_WIDTH = 300      # The width of the robot face
-FACE_HEIGHT = 350     # The height of the robot face
-EYE_DIAMETER = 70     # The diameter of each robot eye
-EYE_Y_OFFSET = 40     # The distance from the top of the head to the top of the eyes
-EYE_X_SEPARATION = 40 # The distance between the left eye and right eye
-MOUTH_WIDTH = 150     # The width of the mouth
-MOUTH_HEIGHT = 30     # The height of the mouth
-MOUTH_Y_OFFSET = 200  # The distance from the top of the head to the top of the mouth
-LABEL_Y = 40          # The distance from the top of the screen to the center of the text
+# The dimensions of the robot face
+FACE_WIDTH = 300
+FACE_HEIGHT = 350
+
+# The diameter of each robot eye
+EYE_DIAMETER = 70
+
+# The distance from the top of the head to the top of the eyes
+EYE_Y_OFFSET = 40
+
+# The distance between the left eye and right eye
+EYE_X_SEPARATION = 40
+
+# The dimensions of the robot mouth
+MOUTH_WIDTH = 150
+MOUTH_HEIGHT = 30
+
+# The distance from the top of the head to the top of the mouth
+MOUTH_Y_OFFSET = 200
+
+# The distance from the top of the screen to the center of the text
+LABEL_Y = 40
+
 
 def main():
-  canvas = Canvas()
-  canvas.set_canvas_title("Draw Robot Face")
+    canvas = Canvas()
+    canvas.set_canvas_title("Robot Face")
 
-  ## Start of your code
-  draw_head(canvas)
-  draw_mouth(canvas)
-  draw_eyes(canvas)
-  draw_label(canvas)
-  ## End of your code
+    draw_head(canvas)
+    draw_mouth(canvas)
+    draw_eyes(canvas)
+    draw_label(canvas)
 
-  canvas.mainloop()
+    canvas.mainloop()
 
-def draw_head(canvas): 
-  x = (canvas.get_canvas_width() - FACE_WIDTH)/2
-  y = (canvas.get_canvas_height() - FACE_HEIGHT)/2
-  head = canvas.create_rectangle(x, y, x + FACE_WIDTH, y + FACE_HEIGHT)
-  canvas.set_fill_color(head, "yellow")
+
+def draw_head(canvas):
+    """
+    Draws a yellow rectangle in the center of the screen representing the head
+    for the robot.
+    """
+    x = (canvas.get_canvas_width() - FACE_WIDTH) / 2
+    y = (canvas.get_canvas_height() - FACE_HEIGHT) / 2
+    head = canvas.create_rectangle(x, y, x + FACE_WIDTH, y + FACE_HEIGHT)
+    canvas.set_fill_color(head, "yellow")
+
 
 def draw_mouth(canvas):
-  head_x = (canvas.get_canvas_width() - FACE_WIDTH)/2
-  head_y = (canvas.get_canvas_height() - FACE_HEIGHT)/2
+    """
+    Draws the mouth as a black rectangle at the appropriate offset from the top of the face,
+    centered horizontally.
+    """
+    head_x = (canvas.get_canvas_width() - FACE_WIDTH) / 2
+    head_y = (canvas.get_canvas_height() - FACE_HEIGHT) / 2
 
-  mouth_x = head_x + (FACE_WIDTH - MOUTH_WIDTH) / 2
-  mouth_y = head_y + MOUTH_Y_OFFSET      # mouth offset is from top of the head
+    mouth_x = head_x + (FACE_WIDTH - MOUTH_WIDTH) / 2
 
-  mouth = canvas.create_rectangle(mouth_x, mouth_y,
-                                  mouth_x + MOUTH_WIDTH, mouth_y + MOUTH_HEIGHT)
-  canvas.set_fill_color(mouth, "black")
+    # mouth offset is from top of the head
+    mouth_y = head_y + MOUTH_Y_OFFSET
+
+    mouth = canvas.create_rectangle(mouth_x, mouth_y,
+                                    mouth_x + MOUTH_WIDTH, mouth_y + MOUTH_HEIGHT)
+    canvas.set_fill_color(mouth, "black")
+
 
 def draw_eyes(canvas):
-  head_y = (canvas.get_canvas_height() - FACE_HEIGHT)/2
-  eye_y = head_y + EYE_Y_OFFSET           # eye offset is from the top of the head
+    """
+    Draws both eyes as filled ovals at the appropriate offset from the top of the face,
+    centered horizontally and spaced apart according to EYE_X_SEPARATION.
+    """
+    head_y = (canvas.get_canvas_height() - FACE_HEIGHT) / 2
 
-  right_x = canvas.get_canvas_width()/2 + EYE_X_SEPARATION/2
-  right_eye = canvas.create_oval(right_x, eye_y,
-                                right_x + EYE_DIAMETER,  eye_y + EYE_DIAMETER)
-  canvas.set_fill_color(right_eye, "green")
+    # eye offset is from the top of the head
+    eye_y = head_y + EYE_Y_OFFSET
 
-  left_x = canvas.get_canvas_width()/2 - EYE_X_SEPARATION/2 - EYE_DIAMETER
-  left_eye = canvas.create_oval(left_x, eye_y,
-                                left_x + EYE_DIAMETER,  eye_y + EYE_DIAMETER)
-  canvas.set_fill_color(left_eye, "blue")
+    eye_x = canvas.get_canvas_width() / 2 - EYE_X_SEPARATION / 2 - EYE_DIAMETER
+    draw_eye(canvas,eye_x, eye_y, "blue")
+
+    eye_x += EYE_DIAMETER + EYE_X_SEPARATION
+    draw_eye(canvas, eye_x, eye_y, "green")
+
+
+def draw_eye(canvas, x, y, color):
+    """
+    Draws an eye (a circle) with top-left corner at the specified location, and with the
+    specified fill color.  The eye is EYE_DIAMETER big.
+    """
+    eye = canvas.create_oval(x, y, x + EYE_DIAMETER, y + EYE_DIAMETER)
+    canvas.set_fill_color(eye, color)
 
 
 def draw_label(canvas):
-  x = canvas.get_canvas_width()/2
-  label = canvas.create_text(x, LABEL_Y, "Robot Face")
-  canvas.set_font(label, "Times New Roman", 44)
+    """
+    Draws a "Robot Face" text label on the screen centered horizontally, in large font.
+    """
+    label = canvas.create_text(canvas.get_canvas_width() / 2, LABEL_Y, "Robot Face")
+    canvas.set_font(label, "Times New Roman", 44)
+
 
 if __name__ == "__main__":
     main()
