ae3d7d961cbd33214639387e8cd664acd1c000f3
Nick Troccoli <nicktroccoli28@gmail.com>
2020-08-10 17:53:54 -0400

    Day 6 examples

--- templates/en/projects/gravityBall/soln.py
+++ templates/en/projects/gravityBall/soln.py
@@ -1,23 +1,30 @@
 """
 File: gravity_ball.py
 -------------------
-A ball bounces as if due to gravity.
+A ball bounces as if due to gravity.  Specifically, each frame the y velocity increases
+downward to simulate gravity, and each time it bounces it loses some of its y velocity
+to simulate damping.
 """
 
 import time
 from graphics import Canvas
 
-SIZE = 20
-DELAY = 0.02    # 20 milliseconds
+BALL_DIAMETER = 20
+ANIMATION_DELAY_SECONDS = 0.02
+
+# The amount to add each frame to the y velocity to simulate the pull of gravity
 GRAVITY = 0.5
+
+# The amount by which to multiply the y velocity each time we bounce (simulates losing energy)
 DAMPING = 0.7
+
 
 def main():
     canvas = Canvas()
     canvas.set_canvas_title("Gravity Ball")
 
     # draw a ball on the top left corner
-    ball = canvas.create_oval(0, 0, SIZE, SIZE)
+    ball = canvas.create_oval(0, 0, BALL_DIAMETER, BALL_DIAMETER)
     canvas.set_color(ball, "black")
 
     # variables for velocity
@@ -29,18 +36,16 @@ def main():
         vy += GRAVITY
 
         # should the ball bounce?
-        if (canvas.get_top_y(ball) > (canvas.get_canvas_height() - SIZE)) \
-                    and (vy > 0):
+        if (canvas.get_top_y(ball) > (canvas.get_canvas_height() - canvas.get_height(ball))) and vy > 0:
+            # Dampen the velocity and invert the velocity
             vy = vy * -DAMPING
 
-        # move
         canvas.move(ball, vx, vy)
-
-        # animation pause. refresh canvas
-        time.sleep(DELAY)
+        time.sleep(ANIMATION_DELAY_SECONDS)
         canvas.update()
 
     canvas.mainloop()
+
 
 if __name__ == "__main__":
     main()
