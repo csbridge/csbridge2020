347391d75e400890f58474797c25d182b0123353
Ondrej Guth <ondrej.guth@fit.cvut.cz>
2020-08-10 21:51:08 +0200

    Slides and code for Lecture 9: removed optional parameters.

--- docs/starter/lectures/Lecture9/checkerboard_adaptable.py
+++ docs/starter/lectures/Lecture9/checkerboard_adaptable.py
@@ -1,23 +1,25 @@
 """
 File: checkerboard.py
 ---------------------
-This program draws a checkerboard.
+This program draws a checkerboard that is as high as the canvas.
 """
 
 from graphics import Canvas
 
-SQUARE_SIZE = 60
+SQUARE_SIZE = 10
 SQUARES = 8
 
 
 def main():
     canvas = Canvas()
     black = True
-    for y in range(SQUARES):
+    y = 0
+    while y * SQUARE_SIZE <= canvas.get_canvas_height():
         for x in range(SQUARES):
             draw_square(canvas, x, y, black)
             black = not black
         black = not black
+        y += 1
     canvas.mainloop()
 
 
