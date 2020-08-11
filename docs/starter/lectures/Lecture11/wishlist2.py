c322f810afe1ddfe8c7b5fdb71950b6f8247fb61
Ondrej Guth <ondrej.guth@fit.cvut.cz>
2020-08-12 00:32:10 +0200

    Lecture 11 (lists) slides and code

---
+++ docs/starter/lectures/Lecture11/wishlist2.py
@@ -0,0 +1,10 @@
+def main():
+    wish1 = input("Enter your wish: ")
+    wish2 = input("Enter your wish: ")
+    # do something with your wishes
+    print("Your wish is " + wish1)
+    print("Your wish is " + wish2)
+
+
+if __name__ == '__main__':
+    main()
